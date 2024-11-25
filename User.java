c53a14f2bd1a5bf19ae2f05f1ba52422a81987a8
EleniPolychronidou <t8230123@aueb.gr>
2024-11-25 23:04:57 +0200

    Basic/startup classes

---
+++ User.java
@@ -0,0 +1,10 @@
+package therapyplanner;
+
+public class User {
+    protected String userId;
+    protected String name;
+    protected String surname;
+    protected String email;
+    protected String phoneNumber; //προαιρετικό
+    protected String password;
+}

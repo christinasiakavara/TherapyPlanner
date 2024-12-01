5f7654014fa97985fb692ace722820678b60ebf8
Eirini Volaki <t8230016@aueb.gr>
2024-12-01 20:50:34 +0200

    refactor: moved classes to src directory, updated gitignore

---
+++ src/main/java/DETsCode/Patient/Patient.java
@@ -0,0 +1,7 @@
+package DETsCode.Patient;
+
+import DETsCode.User.User;
+
+public class Patient extends User {
+    // ???
+}
---
+++ src/main/java/DETsCode/Psychologist/Psychologist.java
@@ -0,0 +1,7 @@
+package DETsCode.Psychologist;
+
+import DETsCode.User.User;
+
+public class Psychologist extends User {
+//    private Schedule schedule; //Να θυμηθώ να φτιάξω κλάση schedule για την διαθεσιμότητα του ψυχολόγου
+}

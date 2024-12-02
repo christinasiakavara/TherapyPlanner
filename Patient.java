bf6855531330d210596fb795496ea9b091cd0300
anastasia-th <t8230040@aueb.gr>
2024-12-02 11:31:06 +0200

    patientchanges2

--- Patient.java
+++ Patient.java
@@ -4,10 +4,11 @@ import java.util.List;
         private String medicalHistory ;
         private int patiendid ;
         private List<TherapySession> currentSessions;
-        public Patient(String firstname, String lastname, String email, String username, String password, String number,String address, String birthdate, String medicalHistory, int patiendid) {
+        public Patient(String firstname, String lastname, String email, String username, String password, String number,String address, String birthdate, String medicalHistory, int patiendid, List <TherapySession> currentSessions) {
             super (firstname,lastname,email,username,password,number,address,birthdate) ;
             this.medicalHistory = medicalHistory ;
             this.patiendid = patiendid ;
+            this.currentSessions = currentSessions;
         }
         public void setMedicalHistory(String medicalHistory) {
             this.medicalHistory = medicalHistory;
@@ -21,12 +22,23 @@ import java.util.List;
         public int getPatiendid() {
             return patiendid ;
         }
-        public void  setTherapySessionn(List <TherapySession> currentSessions) {
-            this.currentSessions= currentSessions;
+    
+        public List<TherapySession> getCurrentSessions() {
+        return this.currentSessions;
         }
-        public List <TherapySession> getTherapySession() {
-            return currentSessions;
-        }
+        public void setCurrentSessions(List<TherapySession> currentSessions) {
+        this.currentSessions = currentSessions;
+         }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " medicalHistory='" + getMedicalHistory() + "'" +
+            ", patiendid='" + getPatiendid() + "'" +
+            ", currentSessions='" + getCurrentSessions() + "'" +
+            "}";
+    }
+
         
     }
 

3c39ecf2a79296af8849c3eca93c37fb47f90e98
anastasia-th <t8230040@aueb.gr>
2024-12-02 12:09:48 +0200

    addpatientchanges3

--- src/main/java/DETsCode/Patient/Patient.java
+++ src/main/java/DETsCode/Patient/Patient.java
@@ -1,7 +1,42 @@
 package DETsCode.Patient;
-
 import DETsCode.User.User;
+import java.util.List;
+    public class Patient extends User {
+        private String medicalHistory ;
+        private int patiendid ;
+        private List<TherapySession> currentSessions;
+        public Patient(String firstname, String lastname, String email, String username, String password, String number,String address, String birthdate,int userid, int roleID, String medicalHistory, int patiendid, List <TherapySession> currentSessions) {
+            super (firstname,lastname,email,username,password,number,address,birthdate,userid, roleID) ;
+            this.medicalHistory = medicalHistory ;
+            this.patiendid = patiendid ;
+            this.currentSessions = currentSessions;
+        }
+        public void setMedicalHistory(String medicalHistory) {
+            this.medicalHistory = medicalHistory;
+        }
+        public  String getMedicalHistory() {
+            return medicalHistory;
+        }
+        public  void setPatiendid(int patiendid) {
+            this.patiendid = patiendid ;
+        }
+        public int getPatiendid() {
+            return patiendid ;
+        }
+    
+        public List<TherapySession> getCurrentSessions() {
+        return this.currentSessions;
+        }
+        public void setCurrentSessions(List<TherapySession> currentSessions) {
+        this.currentSessions = currentSessions;
+         }
 
-public class Patient extends User {
-    // ???
+    @Override
+    public String toString() {
+        return "{" +
+            " medicalHistory='" + getMedicalHistory() + "'" +
+            ", patiendid='" + getPatiendid() + "'" +
+            ", currentSessions='" + getCurrentSessions() + "'" +
+            "}";
+    }
 }
